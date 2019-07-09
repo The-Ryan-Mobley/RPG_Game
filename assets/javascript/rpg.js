dbddb438d25f0a1b566a6f3a83761b7ba9fd6818
The-Ryan-Mobley <49420591+The-Ryan-Mobley@users.noreply.github.com>
2019-07-09 12:10:44 -0500

    modified health

--- assets/javascript/rpg.js
+++ assets/javascript/rpg.js
@@ -1,6 +1,7 @@
 class dude {
     constructor() {
         this.multiplier = 1;
+        this.abilit_power = true;
         this.is_player = false;
         this.health = 0;
         this.attack = 0;
@@ -16,42 +17,42 @@ class dude {
 
         switch (this.profession.toLowerCase()) {
             case "fighter": {
-                this.health = 90;
+                this.health = 190;
                 this.attack = 30;
                 this.counter_attack = 30;
                 this.pic.src = "assets/images/Tordek.jpg";
                 break;
             }
             case "wizard": {
-                this.health = 70;
+                this.health = 120;
                 this.attack = 50;
                 this.counter = 30;
                 this.pic.src = "assets/images/dnd-wizard.jpg";
                 break;
             }
             case "warlock": {
-                this.health = 60;
+                this.health = 160;
                 this.attack = 60;
                 this.counter_attack = 60;
                 this.pic.src = "assets/images/c1034.png";
                 break;
             }
             case "rogue": {
-                this.health = 70;
+                this.health = 140;
                 this.attack = 30;
                 this.counter_attack = 50;
                 this.pic.src = "assets/images/Arcane_trickster_wand.jpg";
                 break;
             }
             case "ranger": {
-                this.health = 80;
+                this.health = 180;
                 this.attack = 40;
                 this.counter_attack = 40;
                 this.pic.src = "assets/images/dnd-class-ranger-195x300.png";
                 break;
             }
             case "cleric": {
-                this.health = 120;
+                this.health = 220;
                 this.attack = 10;
                 this.counter_attack = 20;
                 this.pic.src = "assets/images/Dnd-3.5-cleric-209x300.jpg";
@@ -104,11 +105,28 @@ $(document).ready(function () {
 
         console.log("health: " + b.data("stats").health);
         (b.data("stats").health) -= ((a.data("stats").attack) * (a.data("stats").multiplier));
-        (a.data("stats").health) -= (b.data("stats").counterattack);
+        (a.data("stats").health) -= (b.data("stats").counter_attack);
         console.log(((a.data("stats").attack) * (a.data("stats").multiplier)));
         console.log("health left: " + b.data("stats").health);
+        console.log("player health: " + a.data("stats").health);
         a.data("stats").multiplier++;
         console.log(a.data("stats").multiplier);
+        if((a.data("stats").health <= 0) && (b.data("stats").health <= 0)){
+            a.detach();
+            b.detach();
+            heading.text("YOU DIED");
+        }
+        if(a.data("stats").health <= 0){
+            a.detach();
+            heading.text("YOU DIED");
+        }
+        if(b.data("stats").health <= 0){
+            b.detach();
+            opponent_in_zone = false;
+            heading.text("YOU WON SELECT A NEW OPPONENT");
+
+        }
+        
 
     }
 
