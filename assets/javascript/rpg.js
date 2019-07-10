46f6e2061d635a86d9fed3c01e55819ffc212557
The-Ryan-Mobley <49420591+The-Ryan-Mobley@users.noreply.github.com>
2019-07-10 16:16:09 -0500

    health now updates properly

--- assets/javascript/rpg.js
+++ assets/javascript/rpg.js
@@ -106,7 +106,9 @@ $(document).ready(function () {
         (b.data("stats").health) -= ((a.data("stats").attack) * (a.data("stats").multiplier));
         (a.data("stats").health) -= (b.data("stats").counter_attack);
         a.data("stats").multiplier++;
-        
+        a_hp = a.find(".health-text");
+        a_hp.text("\uD83E\uDDE1 " + a.data("stats").health);
+        b_hp.text("\uD83E\uDDE1 " + b.data("stats").health);
         if((a.data("stats").health <= 0) && (b.data("stats").health <= 0)){
             a.detach();
             b.detach();
@@ -234,7 +236,7 @@ $(document).ready(function () {
         let health_fighter_two = $(".opponent-health");
         if (opponent_in_zone === true) {
             combat(fighter_one, fighter_two);
-            update_health(health_fighter_one,health_fighter_two);
+            
         }
 
 
